4531f4d03ae945eb1e889a755b80e47fad759894
IngSamuelMalave <113445643+IngSamuelMalave@users.noreply.github.com>
2022-09-19 17:11:01 -0400

    Add files via upload

---
+++ Coche.java
@@ -0,0 +1,16 @@
+public class Coche{
+	public static void main(String args[]){
+
+	int puertas = 0;
+	int a = 2;
+	int b = 1;		
+	puertas = a + b;
+
+
+
+  int miCoche = 0;
+  miCoche = puertas + 1;
+  System.out.println("El numero de puertas es " + miCoche);
+
+}
+}
